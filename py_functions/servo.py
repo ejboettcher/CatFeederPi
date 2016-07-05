4f11d1f83022cd18f636f8dcf1f643a9291fc747
Aaron Nielsen <anielsen@integrity-apps.com>
2016-07-04 22:01:28 -0400

    updated servo.py to use docopt command line argument parser

--- py_functions/servo.py
+++ py_functions/servo.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
+"""
+Usage: servo.py [--verbose] [--dry-run] 
+                (--right|--left|--both|--donna|--marilyn)
+                [PORTION-TIME]
+       servo.py [PORTION-TIME]
+                
+       servo.py --help  
+
+Arguments:
+ PORTION-TIME          time allocated for each portion [default: 1.0]
+
+Options:
+ -h --help             show this help message and exit
+ -v --verbose          turn on verbose mode
+ -d --dry-run          dry-run, print out actions, but don't act
+ -r --right --marilyn  feed marilyn on right
+ -l --left --donna     feed donna on left
+ -b --both             feed both
+"""
 
 import time
 import sys
 import optparse
+from docopt import docopt
 try:
     import RPi.GPIO as GPIO
 except ImportError:
@@ -38,57 +58,34 @@ class Servo(GPIO.PWM):
 
 if __name__=='__main__':
 
-    parser = optparse.OptionParser()
-    parser.add_option('-v','--verbose',
-                      action='store_true',
-                      default=False,
-                      help='turn on verbosity')
-    parser.add_option('-d','--dry-run',
-                      dest='dryrun',
-                      action='store_true',
-                      default=False,
-                      help='show selections but do not run servos')
-    parser.add_option('-r','--right','--marilyn',
-                      action='store_true',
-                      default=False,
-                      help='Run right feeder for Donna')
-    parser.add_option('-l','--left', '--donna',
-                      action='store_true',
-                      default=False,
-                      help='Run left for Marilyn')
-    parser.add_option('-b','--both',
-                      action='store_true',
-                      default=False,
-                      help='Feed both')
-    
-    cmdargs,remainder = parser.parse_args()
+    docopt_args = docopt(__doc__)
 
-    feedLeft  = cmdargs.left
-    feedRight = cmdargs.right
-    if cmdargs.both:
+    feedLeft  = docopt_args['--left']
+    feedRight = docopt_args['--right']
+    
+    if docopt_args['--both']:
         feedLeft  = True
         feedRight = True
-
-    try:
-        portionTime = float(remainder[0])
-    except IndexError:
-        portionTime = 1.0
         
-    verbose = cmdargs.verbose
+    verbose = docopt_args['--verbose']
 
-    if cmdargs.dryrun:
+    dryrun = docopt_args['--dry-run']
+    if dryrun:
         verbose=True
 
+    portionTime = docopt_args['PORTION-TIME']
+    if portionTime is None: portionTime = 1.0
+    
     if verbose:
         print "Verbose   = %s"%str(verbose)
         print "FeedRight = %s"%str(feedRight)
         print "FeedLeft  = %s"%str(feedLeft)
-        print "FeedBoth  = %s"%str(cmdargs.both)
-        print "Dry run   = %s"%str(cmdargs.dryrun)
+        print "FeedBoth  = %s"%str(docopt_args['--both'])
+        print "Dry run   = %s"%str(dryrun)
         print "Portion   = %s seconds"%str(portionTime)
         
     # dryrun option selected, exit without running servos
-    if cmdargs.dryrun:
+    if dryrun:
         sys.exit(1)
     
     GPIO.setmode(GPIO.BCM)
